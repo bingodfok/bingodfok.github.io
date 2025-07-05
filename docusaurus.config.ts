7991a3d81d0cb31cb64f0673d5868cfb7739376d
bingodfok <bingodfok@gmail.com>
2025-07-05 23:45:40 +0800

    zh-Hans

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'BinGodFok Blog’s',
+  title: 'Hart',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -73,7 +73,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'BinGodFok',
+      title: 'Hart',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
